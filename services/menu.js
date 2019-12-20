bb84d168763d2f9e6ec815b567879a006ef53640
leroment <andrewcai19972011@gmail.com>
2019-12-20 20:35:28 +1100

    ues

--- services/menu.js
+++ services/menu.js
@@ -11,28 +11,12 @@ module.exports = class Menu {
     let response;
 
     if (payload === "MENU_SELECTED") {
-      //   response = Response.genMultipleGenericTemplate([
-      //     Response.genGenericTemplateElement(
-      //       "https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2016/11/one-pot-paneer-curry-pie.jpg",
-      //       "Set A",
-      //       "$11.00",
-      //       [Response.genPostbackButton("Select Quantity")]
-      //     ),
-      //     Response.genGenericTemplateElement(
-      //       "https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2016/11/one-pot-paneer-curry-pie.jpg",
-      //       "Set B",
-      //       "$13.00",
-      //       [Response.genPostbackButton("Select Quantity")]
-      //     ),
-      //     Response.genGenericTemplateElement(
-      //       "https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2016/11/one-pot-paneer-curry-pie.jpg",
-      //       "Set C",
-      //       "$9.00",
-      //       [Response.genPostbackButton("Select Quantity")]
-      //     )
-      //   ]);
-
-      response = Response.genText("HI!");
+      response = Response.genGenericTemplate(
+        "https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2016/11/one-pot-paneer-curry-pie.jpg",
+        "Set A",
+        "$13.00",
+        Response.genPostbackButton("Select Quantity", "MENU_SET_A")
+      );
     }
 
     return response;
