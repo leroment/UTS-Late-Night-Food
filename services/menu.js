586e4032eb7d3aee67aba7667f717fb69198927e
leroment <andrewcai19972011@gmail.com>
2019-12-20 20:30:08 +1100

    ues

--- services/menu.js
+++ services/menu.js
@@ -11,26 +11,28 @@ module.exports = class Menu {
     let response;
 
     if (payload === "MENU_SELECTED") {
-      response = Response.genMultipleGenericTemplate([
-        Response.genGenericTemplateElement(
-          "https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2016/11/one-pot-paneer-curry-pie.jpg",
-          "Set A",
-          "$11.00",
-          [Response.genPostbackButton("Select Quantity")]
-        ),
-        Response.genGenericTemplateElement(
-          "https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2016/11/one-pot-paneer-curry-pie.jpg",
-          "Set B",
-          "$13.00",
-          [Response.genPostbackButton("Select Quantity")]
-        ),
-        Response.genGenericTemplateElement(
-          "https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2016/11/one-pot-paneer-curry-pie.jpg",
-          "Set C",
-          "$9.00",
-          [Response.genPostbackButton("Select Quantity")]
-        )
-      ]);
+      //   response = Response.genMultipleGenericTemplate([
+      //     Response.genGenericTemplateElement(
+      //       "https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2016/11/one-pot-paneer-curry-pie.jpg",
+      //       "Set A",
+      //       "$11.00",
+      //       [Response.genPostbackButton("Select Quantity")]
+      //     ),
+      //     Response.genGenericTemplateElement(
+      //       "https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2016/11/one-pot-paneer-curry-pie.jpg",
+      //       "Set B",
+      //       "$13.00",
+      //       [Response.genPostbackButton("Select Quantity")]
+      //     ),
+      //     Response.genGenericTemplateElement(
+      //       "https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2016/11/one-pot-paneer-curry-pie.jpg",
+      //       "Set C",
+      //       "$9.00",
+      //       [Response.genPostbackButton("Select Quantity")]
+      //     )
+      //   ]);
+
+      response = Response.genText("HI!");
     }
 
     return response;
