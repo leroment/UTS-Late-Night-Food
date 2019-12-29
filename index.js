611d3e2043b39d87cbc4eaa30d37bfe6003a8eec
leroment <andrewcai19972011@gmail.com>
2019-12-29 15:01:37 +1100

    d

--- index.js
+++ index.js
@@ -10,20 +10,20 @@ const express = require("express"),
   MongoClient = require("mongodb").MongoClient,
   app = express().use(bodyParser.json());
 
-// Sets server port and logs message on sucess
-app.listen(process.env.PORT || 1337, () => console.log("webhook is listening"));
-
+var db;
 // Connect to the db
-MongoClient.connect("mongodb://localhost:27017/utslatenightfood", function(
-  err,
-  db
-) {
-  if (!err) {
-    console.log("Database is connected");
-  } else {
-    console.log("Error with database");
+MongoClient.connect(
+  "mongodb://localhost:27017/utslatenightfood",
+  (err, database) => {
+    // ... start the server
+    if (err) return console.log(err);
+    db = database;
+    // Sets server port and logs message on sucess
+    app.listen(process.env.PORT || 1337, () =>
+      console.log("webhook is listening")
+    );
   }
-});
+);
 
 // Adds support for GET requests to our webhook
 app.get("/webhook", (req, res) => {
