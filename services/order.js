a1f15c22c714e968a2e13d28664b882bc744ffdd
leroment <andrewcai19972011@gmail.com>
2020-01-03 22:33:08 +1100

    d

--- services/order.js
+++ services/order.js
@@ -1,10 +1,10 @@
 "use strict";
 
-const Response = require("./response");
+const Response = require("./response"),
+  Database = require("./database");
 
 let dish;
-
-var response;
+let order = [];
 
 module.exports = class Order {
   static get dish() {
@@ -16,22 +16,40 @@ module.exports = class Order {
   }
 
   static handlePayload(payload, message) {
+    let responses = [];
+
     if (payload === "NUMBEROFORDERS") {
       let quantityOfDish = message;
-      response = Response.genText(
+      let q = Response.genText(
         `You have selected ${quantityOfDish} sets of ${dish}.`
       );
+      console.log(`The dish is ${dish}`);
+
+      order.push({
+        dish: dish,
+        quantity: quantityOfDish
+      });
+
+      let orderSummary = this.generateOrderSummary();
+
+      responses.push(q);
+      responses.push(orderSummary);
+
       dish = "";
+    } else {
+      responses = [];
     }
 
-    return response;
+    return responses;
   }
 
-  generateOrderSummary() {
-    let orderSummary = `Order Summary:\n`;
+  static generateOrderSummary() {
+    let orderSummary = `Here is your order:\n`;
 
-    this._orderItems.forEach(item => {
-      orderSummary += `${item}`;
+    order.forEach((item, index) => {
+      orderSummary += `${index}. ${item.dish} x ${item.quantity}\n`;
     });
+
+    return orderSummary;
   }
 };
