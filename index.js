a40bdd6ad3dfa1b77fe41e93cc3b601a33f9a29f
leroment <andrewcai19972011@gmail.com>
2020-01-16 22:03:46 +1100

    d

--- index.js
+++ index.js
@@ -111,6 +111,38 @@ app.post("/pay", (req, res) => {
   });
 });
 
+app.get("/success", (req, res) => {
+  const payerId = req.query.PayerID;
+  const paymentId = req.query.paymentId;
+
+  const execute_payment_json = {
+    payer_id: payerId,
+    transactions: [
+      {
+        amount: {
+          currency: "AUD",
+          total: "25.00"
+        }
+      }
+    ]
+  };
+
+  paypal.payment.execute(paymentId, execute_payment_json, function(
+    error,
+    payment
+  ) {
+    if (error) {
+      console.log(error.response);
+      throw error;
+    } else {
+      console.log(JSON.stringify(payment));
+      res.send("Success");
+    }
+  });
+});
+
+app.get("/cancel", (req, res) => res.send("Cancelled"));
+
 // Adds support for GET requests to our webhook
 app.get("/webhook", (req, res) => {
   // Your verify token. Should be a random string.
